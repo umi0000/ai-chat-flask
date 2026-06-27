04ab5502f7cc771ce47aca18e081ac8f18fcb33f
umi <15507204561@163.com>
2026-06-27 19:18:38 +0800

    添加了启动端口提示与自定义端口

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app import create_app
 from waitress import serve
-
+PORT = 3724
 app = create_app()
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=3724, threads=4)
+    print(f"已在{PORT}端口启动！")
+    serve(app, host="0.0.0.0", port=PORT, threads=4)
